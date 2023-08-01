d8a9e2cdf7198eed92459e0e1911e98db3bb49d9
Angel D. Munoz <angel.m@xuppay.com>
2023-08-01 16:53:56 -0600

    add fsFediverse types

--- app/src/main/java/me/tunaxor/apps/mandadin/types/Types.kt
+++ app/src/main/java/me/tunaxor/apps/mandadin/types/Types.kt
@@ -1,7 +1,9 @@
 package me.tunaxor.apps.mandadin.types
 
+
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 import java.util.UUID
 
 @Parcelize
@@ -11,5 +13,32 @@ data class Todo(
     val description: String,
     val isDone: Boolean,
     val tags: List<String>,
-): Parcelable
+) : Parcelable
 
+@Parcelize
+@Serializable
+data class FsFedInstance(
+    val name: String,
+    val softwareName: String,
+    val softwareVersion: String,
+    val iconUrl: String,
+    val faviconUrl: String,
+    val themeColor: String
+) : Parcelable
+
+@Parcelize
+@Serializable
+data class MkUser(val username: String, val host: String?, val instance: FsFedInstance?) :
+    Parcelable
+
+@Parcelize
+@Serializable
+data class FsFedNote(
+    val id: String,
+    val createdAt: String,
+    val url: String,
+    val reactions: Map<String, Long>,
+    val text: String,
+    val user: MkUser,
+    val reply: FsFedNote?
+) : Parcelable
