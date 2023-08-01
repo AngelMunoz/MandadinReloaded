207add154a51e9f139219404578f58dd5c31d765
Angel D. Munoz <angel.m@xuppay.com>
2023-08-01 16:51:24 -0600

    add a simple viewmodel for the Fediverse notes page

---
+++ app/src/main/java/me/tunaxor/apps/mandadin/vm/FsFediverse.kt
@@ -0,0 +1,21 @@
+package me.tunaxor.apps.mandadin.vm
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import me.tunaxor.apps.mandadin.services.IFetchFsFedNotes
+import me.tunaxor.apps.mandadin.types.FsFedNote
+
+class FsFedNotesVm(val service: IFetchFsFedNotes) : ViewModel() {
+    var notes: List<FsFedNote> by mutableStateOf(emptyList())
+        private set
+
+    suspend fun fetchNotes(page: Int? = null, limit: Int? = null) {
+        notes = service.find(page ?: 1, limit ?: 10)
+    }
+
+    suspend fun fetchNote(note: String): FsFedNote? {
+        return service.findOne(note)
+    }
+}
