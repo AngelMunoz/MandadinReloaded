591c16c012cf59f3d1713eccc7643482b5152b3c
Angel D. Munoz <angel.m@xuppay.com>
2023-08-01 16:53:39 -0600

    update todo functions

--- app/src/main/java/me/tunaxor/apps/mandadin/ui/components/Todos.kt
+++ app/src/main/java/me/tunaxor/apps/mandadin/ui/components/Todos.kt
@@ -2,6 +2,8 @@ package me.tunaxor.apps.mandadin.ui.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,11 +27,13 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.launch
 import me.tunaxor.apps.mandadin.types.Todo
 import me.tunaxor.apps.mandadin.vm.TodoFormVm
@@ -83,8 +87,6 @@ fun TodoForm(
     }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodoItem(todo: Todo, modifier: Modifier = Modifier, onChecked: (Boolean) -> Unit) {
     Box(
@@ -98,18 +100,16 @@ fun TodoItem(todo: Todo, modifier: Modifier = Modifier, onChecked: (Boolean) -> 
             Text(
                 text = todo.title,
                 modifier = Modifier
-                    .padding(4.dp)
-                    .weight(2f),
+                    .padding(4.dp),
                 textDecoration = textDecoration
             )
             Text(
                 text = "${todo.description.take(20)}...",
                 modifier = Modifier
-                    .padding(4.dp)
-                    .weight(1f),
+                    .padding(4.dp),
                 textDecoration = textDecoration
             )
-            Checkbox(checked = todo.isDone, onCheckedChange = { onChecked(it) })
+            Checkbox(checked = todo.isDone, onCheckedChange = { onChecked(it) }, )
         }
     }
 }
@@ -117,7 +117,10 @@ fun TodoItem(todo: Todo, modifier: Modifier = Modifier, onChecked: (Boolean) -> 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TodoList(
-    todos: List<Todo>, modifier: Modifier = Modifier, onTodoCheckChanged: (Todo, Boolean) -> Unit, onSelectionChanged: (Todo?) -> Unit
+    todos: List<Todo>,
+    modifier: Modifier = Modifier,
+    onTodoCheckChanged: (Todo, Boolean) -> Unit,
+    onSelectionChanged: (Todo?) -> Unit
 ) {
     Surface(
         modifier = modifier
@@ -128,12 +131,16 @@ fun TodoList(
             items(todos, key = { it.id }) { todo ->
                 Row(modifier = Modifier
                     .animateItemPlacement()
+                    .hoverable(interactionSource = remember { MutableInteractionSource() })
                     .clickable {
                         onSelectionChanged(todo)
                     }) {
-                    TodoItem(todo = todo, modifier = Modifier.fillMaxWidth(), onChecked = { checked ->
-                        onTodoCheckChanged(todo, checked)
-                    })
+                    TodoItem(
+                        todo = todo,
+                        modifier = Modifier.fillMaxWidth(),
+                        onChecked = { checked ->
+                            onTodoCheckChanged(todo, checked)
+                        })
                 }
             }
         }
@@ -143,7 +150,7 @@ fun TodoList(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TodoPage(todoPageVm: TodoPageVm) {
+fun TodoPage(todoPageVm: TodoPageVm = viewModel()) {
     val scope = rememberCoroutineScope()
     val scaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = SheetState(
@@ -169,13 +176,14 @@ fun TodoPage(todoPageVm: TodoPageVm) {
             }
         }
     }) {
-        TodoList(todoPageVm.todos, modifier = Modifier.padding(it), onTodoCheckChanged = {
-            todo, checked ->
-            scope.launch {
-                todoPageVm.updateCheck(todo, checked)
-                todoPageVm.loadTodos()
-            }
-        }) { todo ->
+        TodoList(todoPageVm.todos,
+            modifier = Modifier.padding(it),
+            onTodoCheckChanged = { todo, checked ->
+                scope.launch {
+                    todoPageVm.updateCheck(todo, checked)
+                    todoPageVm.loadTodos()
+                }
+            }) { todo ->
             todoPageVm.selectTodo(todo)
             scope.launch {
                 scaffoldState.bottomSheetState.expand()
@@ -183,4 +191,3 @@ fun TodoPage(todoPageVm: TodoPageVm) {
         }
     }
 }
-
