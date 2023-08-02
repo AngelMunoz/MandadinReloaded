e2148645f1b8455e6b6cbdafb160e8824b4eb8bc
Angel D. Munoz <scythes.lol@gmail.com>
2023-08-02 15:27:50 -0600

    Make it work baby!

--- app/src/main/java/me/tunaxor/apps/mandadin/MainActivity.kt
+++ app/src/main/java/me/tunaxor/apps/mandadin/MainActivity.kt
@@ -4,19 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import me.tunaxor.apps.mandadin.ui.components.FsFediverseNotePage
 import me.tunaxor.apps.mandadin.ui.components.FsFediverseNotesPage
 import me.tunaxor.apps.mandadin.ui.components.TodoPage
@@ -24,15 +35,13 @@ import me.tunaxor.apps.mandadin.ui.theme.MandadinTheme
 import me.tunaxor.apps.mandadin.vm.FsFedNotesVm
 import me.tunaxor.apps.mandadin.vm.TodoPageVm
 
-
-fun NavGraphBuilder.fsFediverseNotes(navController: NavController, iFsFed: IFsFedNotes) {
-    val vm = FsFedNotesVm(service = iFsFed.iFsFedNotes)
-    navigation(startDestination = "notes", route = "fediverse") {
+fun NavGraphBuilder.fsFediverseNotes(nav: NavController, env: IFsFediverse) {
+    val vm = FsFedNotesVm(service = env.FsFediverse)
+    navigation(route = "fediverse", startDestination = "notes") {
         composable(
-            "notes?page={page}&limit={limit}", arguments = listOf(
-                navArgument("page") { defaultValue = 1; type = NavType.IntType },
-                navArgument("limit") { defaultValue = 10; type = NavType.IntType },
-            )
+            route = "notes?page={page}&limit={limit}",
+            arguments = listOf(navArgument("page") { type = NavType.IntType; defaultValue = 10 },
+                navArgument("limit") { type = NavType.IntType; defaultValue = 1 }),
         ) {
             LaunchedEffect(true) {
                 vm.fetchNotes(
@@ -40,35 +49,74 @@ fun NavGraphBuilder.fsFediverseNotes(navController: NavController, iFsFed: IFsFe
                     limit = it.arguments?.getInt("limit") ?: 10
                 )
             }
-            FsFediverseNotesPage(vm = vm)
+            FsFediverseNotesPage(nav, vm)
         }
         composable(
-            "notes/{note}", arguments = listOf(navArgument("note") { type = NavType.StringType })
+            route = "notes/{note}",
+            arguments = listOf(navArgument("note") { type = NavType.StringType; nullable = true }),
         ) {
-            FsFediverseNotePage(it.arguments?.getString("page"), vm)
+            FsFediverseNotePage(it.arguments?.getString("note"), vm)
         }
+
     }
 }
 
-fun NavGraphBuilder.todos(navController: NavController, todos: ITodos) {
-    val vm = TodoPageVm(todos.iTodos)
-    composable("todos") {
+fun NavGraphBuilder.todos(iTodos: ITodos) {
+    val vm = TodoPageVm(iTodos.Todos)
+    composable(route = "todos") {
         TodoPage(vm)
     }
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppShell(appEnv: ApplicationEnv) {
     MandadinTheme {
         val ctrl = rememberNavController()
         // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-        ) {
-            NavHost(navController = ctrl, startDestination = "todos") {
-                todos(ctrl, appEnv)
-                fsFediverseNotes(navController = ctrl, appEnv)
+        Scaffold(topBar = {
+            CenterAlignedTopAppBar(title = {
+                Text(
+                    text = "Mandadin",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp)
+                )
+            }, actions = {
+                IconButton(onClick = {
+                    ctrl.navigate("todos")
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = "Todos",
+                        modifier = Modifier.padding(4.dp),
+                        tint = MaterialTheme.colorScheme.outline
+                    )
+                }
+                IconButton(onClick = {
+                    ctrl.navigate("fediverse")
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.List,
+                        contentDescription = "Fediverse Notes",
+                        modifier = Modifier.padding(4.dp),
+                        tint = MaterialTheme.colorScheme.outline
+                    )
+                }
+            })
+        }) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                NavHost(navController = ctrl, startDestination = "todos") {
+                    todos(iTodos = appEnv)
+                    fsFediverseNotes(nav = ctrl, env = appEnv)
+                }
             }
         }
     }
@@ -77,7 +125,7 @@ fun AppShell(appEnv: ApplicationEnv) {
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val env: ApplicationEnv = AppEnv()
+        val env: ApplicationEnv = AppEnv(BuildConfig.SERVER_URL)
         setContent { AppShell(env) }
     }
 }
