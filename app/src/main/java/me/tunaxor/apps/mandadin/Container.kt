ba04832f5aaa81efc8af54f23693d3a0da95793a
Angel D. Munoz <scythes.lol@gmail.com>
2023-08-02 21:10:27 -0600

    make serialization work

--- app/src/main/java/me/tunaxor/apps/mandadin/Container.kt
+++ app/src/main/java/me/tunaxor/apps/mandadin/Container.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import me.tunaxor.apps.mandadin.services.*
 
 interface ITodos {
@@ -19,7 +20,10 @@ class AppEnv(private val baseUrl: String) : ApplicationEnv {
 
     private val client  = HttpClient(CIO) {
         install(ContentNegotiation) {
-            json()
+            json(Json {
+                ignoreUnknownKeys = true
+                isLenient = true
+            })
         }
     }
 
