a798e021eb95625ed2f8adc4567116f869f881f4
Angel D. Munoz <scythes.lol@gmail.com>
2023-08-04 09:39:06 -0600

    Render the markdown content

--- app/src/main/java/me/tunaxor/apps/mandadin/ui/components/FsFediverse.kt
+++ app/src/main/java/me/tunaxor/apps/mandadin/ui/components/FsFediverse.kt
@@ -1,18 +1,25 @@
 package me.tunaxor.apps.mandadin.ui.components
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -26,10 +33,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import dev.jeziellago.compose.markdowntext.MarkdownText
 import kotlinx.coroutines.launch
 import me.tunaxor.apps.mandadin.types.FsFedNote
 import me.tunaxor.apps.mandadin.vm.FsFedNotesVm
 import me.tunaxor.apps.mandadin.vm.NotesPageDirection
+import java.text.DateFormat
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 
 @Composable
@@ -64,6 +76,43 @@ fun FsNoteNav(onBackRequested: () -> Unit) {
 }
 
 @Composable
+fun FsNoteHeader() {
+
+}
+
+@Composable
+fun FsNoteFooter(
+    title: String, postedAt: String, noteId: String, onNoteRequested: (String) -> Unit
+) {
+
+    Row {
+        TextButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp)
+                .weight(2f),
+            onClick = { onNoteRequested(noteId) },
+            colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.primary)
+        ) {
+            Text("${title.take(20)}...")
+        }
+        TextButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp)
+                .weight(1f),
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.secondary)
+        ) {
+            val parsedDate = OffsetDateTime.parse(postedAt, DateTimeFormatter.ISO_DATE_TIME)
+            val text = DateFormat.getDateInstance(DateFormat.MEDIUM).format(Date.from(parsedDate.toInstant()))
+            Text(text = text)
+        }
+    }
+
+}
+
+@Composable
 fun FsFediverseNoteView(
     note: FsFedNote, modifier: Modifier = Modifier, onNoteRequested: (String) -> Unit
 ) {
@@ -73,18 +122,33 @@ fun FsFediverseNoteView(
             .padding(2.dp)
     ) {
         Column {
-            Text(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(8.dp), text = note.text
+            if (note.reply != null) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .border(
+                            2.dp,
+                            color = MaterialTheme.colorScheme.secondary,
+                            shape = RoundedCornerShape(4.dp)
+                        )
+                ) {
+                    FsFediverseNoteView(note = note.reply, onNoteRequested = onNoteRequested)
+                }
+            }
+
+            MarkdownText(
+                markdown = note.text,
+                modifier = Modifier.padding(8.dp),
+                color = MaterialTheme.colorScheme.onSurface
             )
 
-            TextButton(modifier = Modifier
-                .fillMaxWidth()
-                .padding(2.dp),
-                onClick = { onNoteRequested(note.id) }) {
-                Text("${note.text.take(20)}...")
-            }
+            FsNoteFooter(
+                title = note.text,
+                postedAt = note.createdAt,
+                noteId = note.id,
+                onNoteRequested = onNoteRequested
+            )
         }
 
     }
@@ -93,6 +157,9 @@ fun FsFediverseNoteView(
 
 @Composable
 fun FsFediverseNotesPage(navController: NavController, vm: FsFedNotesVm = viewModel()) {
+    BackHandler {
+        navController.navigateUp()
+    }
     LazyColumn {
         items(vm.notes.toList(), key = { it.id }) { note ->
             FsFediverseNoteView(note = note,
@@ -106,9 +173,16 @@ fun FsFediverseNotesPage(navController: NavController, vm: FsFedNotesVm = viewMo
 
 @Composable
 fun FsFediverseNotePage(
-    noteId: String?, vm: FsFedNotesVm = viewModel(), onNoteRequested: (String) -> Unit
+    noteId: String?,
+    vm: FsFedNotesVm = viewModel(),
+    onBackRequested: () -> Unit = {},
+    onNoteRequested: (String) -> Unit
 ) {
     var note: FsFedNote? by remember { mutableStateOf(null) }
+
+    BackHandler {
+        onBackRequested()
+    }
 
     LaunchedEffect(true) {
         if (noteId != null) note = vm.fetchNote(noteId)
